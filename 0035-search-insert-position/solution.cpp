a0320ad606f6fca4a14cd4170a152fb534fd5438
arjunnnn <bartariyaarjun@gmail.com>
2026-02-24 04:00:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.5 MB (98.73%)

---
+++ 0035-search-insert-position/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int low=0,high=arr.size()-1,mid;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
