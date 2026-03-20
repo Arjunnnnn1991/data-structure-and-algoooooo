d8f9ebe20b360f76f905a12f8152b782cb7155cc
arjunnnn <bartariyaarjun@gmail.com>
2026-03-20 18:41:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (71.80%)

--- 0162-find-peak-element/solution.cpp
+++ 0162-find-peak-element/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-
     int findPeakElement(vector<int>& nums) {
-        int l = 0;
-        int r = nums.size() - 1;
-
-        while(l < r){
-            int mid = l + (r - l) / 2;
-
-            if(nums[mid] > nums[mid+1])
-                r = mid;
-            else
-                l = mid + 1;
+        int low=0,high=nums.size()-1,mid;
+        
+        while(high>low){
+            mid=low+(high-low)/2;
+            
+            if(nums[mid]>nums[mid+1]){
+                high=mid;
+            }
+            else low=mid+1;
         }
-
-        return l;
+        return low;
     }
 };
