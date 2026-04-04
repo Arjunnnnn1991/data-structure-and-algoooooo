d53d4ca014886ee66b88f95ce15f67af2d5ff0eb
arjunnnn <bartariyaarjun@gmail.com>
2026-04-04 17:47:36 +0000

    Sync LeetCode submission Runtime - 212 ms (5.03%), Memory - 24 MB (96.26%)

---
+++ 2502-sort-the-people/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        for(int i=0;i<names.size()-1;i++){
+            for(int j=0;j<names.size()-1;j++){
+                if(heights[j]<heights[j+1]){
+                    swap(heights[j],heights[j+1]);
+                    swap(names[j],names[j+1]);
+                }
+            }
+        }
+        return names;
+    }
+};
