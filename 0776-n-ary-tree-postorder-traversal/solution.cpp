7ff9e532a15113cf70335fa3d0daaf598cc63a73
arjunnnn <bartariyaarjun@gmail.com>
2026-04-09 14:12:58 +0000

    Sync LeetCode submission Runtime - 8 ms (91.25%), Memory - 15.7 MB (48.01%)

---
+++ 0776-n-ary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void showme(Node* root,vector<int> &ans){
+        if(root==NULL) return;
+        for(int i=0;i<(root->children.size());i++){
+            showme((root->children[i]),ans);
+           
+            
+        }
+        ans.push_back(root->val);
+        return;
+    }
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        showme(root,ans);
+        return ans;
+    }
+};
