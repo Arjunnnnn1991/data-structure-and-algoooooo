b74955e2291ebc1b427f6f75d7b5d3d288b065d3
arjunnnn <bartariyaarjun@gmail.com>
2026-04-16 11:57:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25 MB (27.30%)

---
+++ 0669-trim-a-binary-search-tree/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void doto (TreeNode* root,int lo,int hi){
+        if(root==NULL) return;
+        while(root->left){
+            if(root->left->val>hi) root->left=root->left->left;
+            else if(root->left->val<lo) root->left=root->left->right;
+            else break;
+        }
+          while(root->right){
+            if(root->right->val>hi) root->right=root->right->left;
+            else if(root->right->val<lo) root->right=root->right->right;
+            else break;
+        }
+        doto(root->left,lo,hi);
+        doto(root->right,lo,hi);
+
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+     TreeNode* dummy=new TreeNode(INT_MAX);
+     dummy->left=root;
+     doto(dummy,low,high)   ;
+     return dummy->left;
+    }
+};
