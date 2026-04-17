b15482fb6745af925ee383ef63b031bc63fa2b16
arjunnnn <bartariyaarjun@gmail.com>
2026-04-17 12:31:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 59.4 MB (57.94%)

--- 0784-insert-into-a-binary-search-tree/solution.cpp
+++ 0784-insert-into-a-binary-search-tree/solution.cpp
@@ -11,52 +11,30 @@
  */
 class Solution {
 public:
-    //  void dotask(TreeNode* root,int val){
-        
-    //     if(root->val>val&&root->left==NULL) {
-    //         root->left=new TreeNode(val);
-    //         return;
-    //     }
-    //     else if(root->val<val&&root->right==NULL){
-    //         root->right=new TreeNode(val);
-    //         return;
-    //     }
-    //     else if(root->val>val&&val>root->left->val){
-    //         TreeNode* temp=root->left;
-    //         root->left=new TreeNode(root->val);
-    //         root->left->left=temp;
-    //         return;
+     void doforme(TreeNode* root,int val){
+            if(root==NULL) return ;
+        if(root->val>val){
+            if(!root->left){
+                root->left=new TreeNode(val);
 
-    //     }
-    //      else if(root->val<val&&val<root->right->val){
-    //         TreeNode* temp=root->right;
-    //         root->right=new TreeNode(root->val);
-    //         root->right->right=temp;
-    //         return;
-
-    //     }
-    //     else if(root->val>val) {
-    //         dotask(root->left,val);
-    //     }
-    //     else{
-    //         dotask(root->right,val);
-    //     }
-    //     return;
-     
+            }
+            else{
+                doforme(root->left,val);
+            }
+        }
+        else{
+            if(root->right==NULL){
+                root->right=new TreeNode(val);
+            }
+            else{
+                doforme(root->right,val);
+            }
+        }
+        return ;
+     }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-       if(root==NULL) return new TreeNode(val);
-       if(root->val>val){
-        if(root->left==NULL){
-            root->left=new TreeNode(val);
-        }
-        else insertIntoBST(root->left,val);
-       }
-       else{
-        if(root->right==NULL) {
-            root->right=new TreeNode(val);
-        }
-        else insertIntoBST(root->right,val); 
-       }
-       return root;
+        if(root==NULL) return new TreeNode(val);
+        doforme(root,val);
+        return root;
     }
 };
