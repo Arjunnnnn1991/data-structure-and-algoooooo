e13355c7c632c70e4105028658db5c555adaee5e
arjunnnn <bartariyaarjun@gmail.com>
2026-04-21 07:29:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.2 MB (31.43%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size()/2;
-       return nums[n];
-        
+    int majorityElement(vector<int>& arr) {
+        int more=1;
+        int cur=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(cur!=arr[i]){
+                more--;
+                if(more<=0 ){
+                    cur=arr[i];
+                    more=1;
+
+                }
+            }
+            else more++;
+        }
+        return cur;
     }
 };
