3cf7c08076700a72f5cf84cd4dd7002fc7c740ca
arjunnnn <bartariyaarjun@gmail.com>
2026-04-25 14:56:32 +0000

    Sync LeetCode submission Runtime - 3 ms (25.20%), Memory - 60.2 MB (13.20%)

--- 1586-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
+int todo(vector<int>& arr,int k){
+    int i=0,j=0,ans=0,cur=0;
+    while(j<arr.size()){
+        if(arr[j]==0)cur++;
+
+        while(cur>k){
+            if(arr[i]==0) cur--;
+            i++;
+
+        }
+      ans=max(ans,j-i );
+        j++;
+    }
+    return ans;
+}
     int longestSubarray(vector<int>& arr) {
-          int n=arr.size();
-          int i=0,j=0,maxlen=INT_MIN,len=0;
-          int flips=0;
-          while(j<n){
-            if(arr[j]==1) j++;
-            else{
-                if(flips<1){
-                    flips++;
-                    j++;
-                }
-                else{
-                    len=j-i;
-                    maxlen=(maxlen<len)?len:maxlen;
-                    while(arr[i]==1) i++;
-                    j++;
-                    i++;
-                }
-            }
-            len=j-i;
-            maxlen=(maxlen<len)?len:maxlen;
-          }
-          return maxlen-1;
+        //   int n=arr.size();
+        //   int i=0,j=0,maxlen=INT_MIN,len=0;
+        //   int flips=0;
+        //   while(j<n){
+        //     if(arr[j]==1) j++;
+        //     else{
+        //         if(flips<1){
+        //             flips++;
+        //             j++;
+        //         }
+        //         else{
+        //             len=j-i;
+        //             maxlen=(maxlen<len)?len:maxlen;
+        //             while(arr[i]==1) i++;
+        //             j++;
+        //             i++;
+        //         }
+        //     }
+        //     len=j-i;
+        //     maxlen=(maxlen<len)?len:maxlen;
+        //   }
+        //   return maxlen-1;
+        return todo(arr,1);
     }
 
 };
