21b251329aadcf6aa89395fa18f7270903131f02
arjunnnn <bartariyaarjun@gmail.com>
2026-04-25 17:59:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (59.22%)

---
+++ 0799-minimum-distance-between-bst-nodes/solution.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int predi(TreeNode* root){
+        if(root==NULL) return INT_MAX;
+        if(!root->left) return INT_MAX;
+        else{
+            root=root->left;
+            while(root->right){
+                root=root->right;
+            }
+            return root->val;
+        }
+    }
+    int succi(TreeNode* root){
+        if(root==NULL) return INT_MAX;
+        if(!root->right) return INT_MAX;
+        else{
+            root=root->right;
+            while(root->left){
+                root=root->left;
+            }
+            return root->val;
+        }
+    }
+    void getmin(TreeNode*root,int& mini){
+        if(root==NULL) return ;
+        int x= succi(root);
+        int y=predi(root);
+        y=min(abs(root->val-x),abs(root->val-y));
+        getmin(root->left,mini);
+        getmin(root->right,mini);
+
+        mini=min(mini,y);
+        return;
+
+    }
+    int minDiffInBST(TreeNode* root) {
+        int mini=INT_MAX;
+   getmin(root,mini);
+   return mini;
+    }
+};
