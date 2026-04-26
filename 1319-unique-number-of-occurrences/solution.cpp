7a247a4a890e12912188f0d3344e5d9619a3a059
arjunnnn <bartariyaarjun@gmail.com>
2026-04-26 16:55:45 +0000

    Sync LeetCode submission Runtime - 4 ms (4.78%), Memory - 11.8 MB (83.99%)

---
+++ 1319-unique-number-of-occurrences/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]]++;
+        }
+        for(auto x: m){
+            int fre=x.second;
+            if(s.find(fre)!=s.end()) return false;
+            else s.insert(fre);
+        }
+        return true;
+    }
+};
