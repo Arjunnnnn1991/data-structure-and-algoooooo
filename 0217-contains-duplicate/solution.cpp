ffeba7ad5fe08c83f781aafb8bab2be7441da9d3
arjunnnn <bartariyaarjun@gmail.com>
2026-04-29 10:15:43 +0000

    Sync LeetCode submission Runtime - 56 ms (33.22%), Memory - 90.7 MB (64.62%)

--- 0217-contains-duplicate/solution.cpp
+++ 0217-contains-duplicate/solution.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if(nums.size()==1) return false;
-        else if(nums.size()==2){
-            return (nums[0]==nums[1])? true:false;
-        }
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size()-1;i++){
-            if(nums[i]==nums[i-1]||nums[i]==nums[i+1]) return true;
-        }
-        return false;
+       unordered_set<int> s;
+       for(int i=0;i<nums.size();i++){
+        if(s.find(nums[i])!=s.end()) return true;
+        else s.insert(nums[i]);
+       }
+       return false;
     }
 };
