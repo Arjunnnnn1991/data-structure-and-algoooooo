d82e0f84b75c7d78a3868ac40a193b2a6fc45dfc
arjunnnn <bartariyaarjun@gmail.com>
2026-05-03 11:19:41 +0000

    Sync LeetCode submission Runtime - 41 ms (30.79%), Memory - 61.6 MB (68.13%)

--- 0215-kth-largest-element-in-an-array/solution.cpp
+++ 0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // for(int i=0;i<k;i++){
-        //     for(int j=0;j<nums.size()-1;j++){
-        //         if(nums[j]>nums[j+1]){
-        //             swap(nums[j],nums[j+1]);
-        //         }
+        // int maxi=nums[0];
+        // for(int i=0;i<nums.size();i++){
+        //     if(maxi<nums[i]){
+        //         maxi=nums[i];
         //     }
         // }
-        // return nums[nums.size()-k];
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()-k];
+        // vector<int> helper(maxi+1,0);
+        // for(int i=0;i<nums.size();i++){
+        //     helper[nums[i]]++;
+        // }
+        // int suppo=k,j=maxi;
+        // while(suppo>0){
+        //     if(helper[j]==0){
+        //         j--;
+        //     }
+        //     else{
+        //         helper[j]--;
+        //         suppo--;
+        //     }
+        // }
+        // return j;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k)pq.pop();
+        }
+        return pq.top();
     }
 };
