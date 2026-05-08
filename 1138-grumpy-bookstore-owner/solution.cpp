a0db0cb73c33524d1aea6c3750aa40d3502cabac
arjunnnn <bartariyaarjun@gmail.com>
2026-05-08 18:46:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 35.8 MB (74.07%)

--- 1138-grumpy-bookstore-owner/solution.cpp
+++ 1138-grumpy-bookstore-owner/solution.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int maxSatisfied(vector<int>& arr, vector<int>& grumpy, int minutes) {
-        int ei=arr.size()-1,i=0,curans=0,maxans=0,j=minutes;
-        for(i=0;i<=ei;i++){
-            if(grumpy[i]==0){
-                maxans+=arr[i];
-            }
+    int maxSatisfied(vector<int>& arr, vector<int>& g, int m) {
+       
+       int ans=0;
+       for(int i=0;i<arr.size();i++){
+        if(g[i]==0) ans+=arr[i];
+       }
+       int cursome=0;
+       int temp=0;
+       int i=0,j=0;
+       while(j<arr.size()){
+        if(g[j]==1){
+            cursome+=arr[j];
         }
-        curans=maxans;
-        for(int i=0;i<minutes;i++){
-            if(grumpy[i]==1){
-                curans+=arr[i];
-            }
-        }
-        i=0;
-        maxans=max(curans,maxans);
-        while(j<=ei){
-            if(grumpy[j]==1){
-                curans+=arr[j];
-            }
-            if(grumpy[i]==1){
-                curans-=arr[i];
-            }
-            maxans=max(maxans,curans);
-            j++;
-            i++;
-        }
-        return maxans;
-
-
+        if(j-i>=m) {if(g[i]==1)cursome-=arr[i];
+         i++;}
+        temp=max(temp,cursome);
+       
+        j++;
+       }
+  
+       return ans+temp;
     }
 };
