31401916de6e6e7e0754d3663d5d029448bee5f8
arjunnnn <bartariyaarjun@gmail.com>
2026-05-17 07:39:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (59.33%)

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool check(string a,string b){
+        int m=b.size();
+        int i=1,len=0;
+        vector<int> lps(m,0);
+        while(i<m){
+            if(b[i]==b[len]){
+                len++;
+                lps[i++]=len;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        i=0;
+        int j=0;
+        while(i<a.size()){
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }
+            if(j==m&&i-j==0) return true;
+            else if(a[i]!=b[j]){
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+    int isPrefixOfWord(string s, string searchWord) {
+      stringstream ss(s);
+      int count=1;
+      string word;
+      while(ss>>word){
+        if(check(word,searchWord)) return count;
+        else count++;
+      }
+      return -1;
+    }
+};
