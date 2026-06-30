b3489defe0a12292a6e73e104b5c389682c3d2ff
arjunnnn <bartariyaarjun@gmail.com>
2026-06-30 16:18:19 +0000

    Sync LeetCode submission Runtime - 3 ms (88.76%), Memory - 11.1 MB (51.64%)

--- 1460-number-of-substrings-containing-all-three-characters/solution.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> count(3,0);
-        int i=0,j=0;
         int ans=0;
-        while(j<s.size()){
-            char a=s[j];
-            if(a=='a') count[0]++;
-            else if(a=='b') count[1]++;
-            else count[2]++;
-            while(i+1<j){
-                if(count[0]>0&&count[1]>0&&count[2]>0){
+        int i=0,j=0;
+    vector<int> arr(3,0);
+    while(j<s.size()){
+        if(s[j]=='a')arr[0]++;
+        else if(s[j]=='b')arr[1]++;
+        else if(s[j]=='c')arr[2]++;
+        if(arr[0]&&arr[1]&&arr[2]){
+            // ans+=s.size()-j-1;
+            while(arr[0]&&arr[1]&&arr[2]){
+                if(s[i]=='a')arr[0]--;
+                else if(s[i]=='b')arr[1]--;
+                else if(s[i]=='c')arr[2]--;
                 ans+=s.size()-j;
-                char b=s[i];
-            if(b=='a') count[0]--;
-            else if(b=='b') count[1]--;
-            else count[2]--;
-            i++;
+                i++;
             }
-            else break;
-            }
-            j++;
-
-
         }
-        return ans;
+        j++;
+    }
+    return ans;
     }
 };
