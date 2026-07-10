3b4bebf6dc43ce40fb5f3b2053b14988dd5bdf7b
arjunnnn <bartariyaarjun@gmail.com>
2026-07-10 06:41:39 +0000

    Sync LeetCode submission Runtime - 3 ms (65.84%), Memory - 8.2 MB (10.88%)

--- 1013-fibonacci-number/solution.cpp
+++ 1013-fibonacci-number/solution.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    // int fibo(int n,vector<int>& dp){
+    //     if(n<=1) return n;
+    //     if(dp[n]) return dp[n];
+    //     int ans=fibo(n-1,dp)+fibo(n-2,dp);
+    //     dp[n]=ans;
+    //     return dp[n];
+        
+    // }
     int fib(int n) {
+        vector<int> dp(n+1,0);
         if(n==0) return 0;
-        else if(n==1) return 1;
-        else return fib(n-1)+fib(n-2);
+        // return fibo(n,dp);
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
