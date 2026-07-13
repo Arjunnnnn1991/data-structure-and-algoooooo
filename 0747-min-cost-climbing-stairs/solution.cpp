de5a9f3b8199bb682c8759eed3a406b3dab22666
arjunnnn <bartariyaarjun@gmail.com>
2026-07-13 16:28:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (74.28%)

--- 0747-min-cost-climbing-stairs/solution.cpp
+++ 0747-min-cost-climbing-stairs/solution.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int help(vector<int>& cost, int i, vector<int>& dp) {
-    if (i >= cost.size()) return 0;
-    if (dp[i] != -1) return dp[i];
-    return dp[i] = cost[i] + min(help(cost, i+1, dp), help(cost, i+2, dp));
-}
+//     int help(vector<int>& cost, int i, vector<int>& dp) {
+//     if (i >= cost.size()) return 0;
+//     if (dp[i] != -1) return dp[i];
+//     return dp[i] = cost[i] + min(help(cost, i+1, dp), help(cost, i+2, dp));
+// }
 int minCostClimbingStairs(vector<int>& cost) {
-    vector<int> dp(cost.size(), -1);
-    return min(help(cost, 0, dp), help(cost, 1, dp));
+    // vector<int> dp(cost.size(), -1);
+    // return min(help(cost, 0, dp), help(cost, 1, dp));
+    if(cost.size()==1||cost.size()==1) return 0;
+    else
+    {
+        for(int i=2;i<cost.size();i++){
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+    }
+    return min(cost[cost.size()-2],cost[cost.size()-1]);
+   
 }
 };
