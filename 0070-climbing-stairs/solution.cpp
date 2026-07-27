f383267a4daa92fd07077019be5293268f20c22e
arjunnnn <bartariyaarjun@gmail.com>
2026-07-27 15:59:49 +0000

    Sync LeetCode submission Runtime - 2 ms (5.12%), Memory - 8.5 MB (38.65%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-
-int move(int n,vector<int>& fromnow){
-    if(n==0||n==1){
-        
-        return 1;
+    int solver(vector<int>&dp,int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        else return dp[n]=solver(dp,n-2)+solver(dp,n-1);
     }
-    else if(n<0){
-        return 0;
-    }
-    if(fromnow[n]!=-1) return fromnow[n];
-    else{
-        fromnow[n]=move(n-1,fromnow)+move(n-2,fromnow);
-    }
-    return fromnow[n];
-
-    
-}
     int climbStairs(int n) {
-        vector<int> fromnow(n+1,-1);
-        return move(n,fromnow);
+        vector<int> dp(n+1,-1);
+        // return solver(dp,n);
+     dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
     }
 };
